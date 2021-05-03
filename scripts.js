13190128a812f87bf792e4eaf31d1dca1bee276d
[Shahin Huseynzade] <[shahin.huseynzade@gmail.com]>
2021-05-03 21:44:29 +0400

    Adding passcode

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let flash;
+flash = prompt("Please, enter the password", "");
+const frfrfr = "1234s";
+if (frfrfr == flash) {
+  alert("Correct password, click ok to enter");
+} else {
+  alert("Wrong Password");
+  window.location = "https://www.linkedin.com/in/shahinhsynzd/";
+}
