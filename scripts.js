cc81498cecbd8942fe574a470ea5ec5ac88f01bf
[Shahin Huseynzade] <[shahin.huseynzade@gmail.com]>
2021-05-20 14:58:48 +0400

    js changes

--- scripts.js
+++ scripts.js
@@ -4,6 +4,6 @@ const frfrfr = "1234s";
 if (frfrfr == flash) {
   alert("CORRECT PASSWORD! CLICK OK TO ENTER");
 } else {
-  alert("WRONG PASSWORD!");
+  alert("WRONG PASSWORD! REDIRECTING TO LINKEDIN!");
   window.location = "https://www.linkedin.com/in/shahinhsynzd/";
 }
