fc8cc0dc832663114feb4791739b00429a95639b
[Shahin Huseynzade] <[shahin.huseynzade@gmail.com]>
2021-06-09 13:11:20 +0400

    Changes to HTML and JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 let flash;
-flash = prompt("PLEASE, ENTER THE PASSWORD", "");
+flash = prompt("LÜTFƏN, ŞİFRƏNİ DAXİL EDİN / PLEASE, ENTER THE PASSWORD", "");
 const frfrfr = "1234s";
 if (frfrfr == flash) {
-  alert("CORRECT PASSWORD! CLICK OK TO ENTER");
+  alert(
+    "DÜZGÜN ŞİFRƏ. DAXİL OLMAQ ÜÇÜN OK DÜYƏMSİNƏ BASIN / CORRECT PASSWORD! CLICK OK TO ENTER"
+  );
 } else {
-  alert("WRONG PASSWORD! REDIRECTING TO LINKEDIN!");
+  alert(
+    "ŞİFRƏ DÜZGÜN DEYİL! LİNKEDİNƏ YÖNLƏNDİRİLİR / WRONG PASSWORD! REDIRECTING TO LINKEDIN!"
+  );
   window.location = "https://www.linkedin.com/in/shahinhsynzd/";
 }
