d58a68ea3c37e2c1a45fac5b631c6e05fb5934c5
[Shahin Huseynzade] <[shahin.huseynzade@gmail.com]>
2021-06-09 13:17:11 +0400

    Minor changes to JS

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ flash = prompt("LÜTFƏN, ŞİFRƏNİ DAXİL EDİN / PLEASE, ENTER THE PASSWORD"
 const frfrfr = "1234s";
 if (frfrfr == flash) {
   alert(
-    "DÜZGÜN ŞİFRƏ. DAXİL OLMAQ ÜÇÜN OK DÜYƏMSİNƏ BASIN / CORRECT PASSWORD! CLICK OK TO ENTER"
+    "DÜZGÜN ŞİFRƏ! DAXİL OLMAQ ÜÇÜN 'OK' DÜYƏMSİNƏ BASIN / CORRECT PASSWORD! CLICK 'OK' TO ENTER"
   );
 } else {
   alert(
